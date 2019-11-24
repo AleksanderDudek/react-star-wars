b83893615c47e83317e54d7d1e6b405d3b77cc42
Aleksander Dudek <aleksander.dudek@tieto.com>
2019-11-24 20:32:06 +0100

    Cleared and checked packages

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -6,7 +6,6 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import Welcome from './welcome/Welcome';
 import Game from './game/Game';
 
-
 function App(): JSX.Element {
 // tslint:disable-next-line:indent
   return <Router>
